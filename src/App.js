628abe1fefddfdc2a4e36467934d055f159a0b9b
Muhammad Awais <ranaawais6046@gmail.com>
2022-03-04 18:35:43 +0500

    abc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Route, Routes, Link, useParams } from 'react-router-dom';
+
+import { useEffect, useState } from 'react';
+
+
+import { Launcher } from 'react-chat-window'
+
+
+import './App.css';
+export default () => {
+
+
+    let [name, setName] = useState('');
+
+    let [messageList, setMessageList] = useState(JSON.parse(localStorage.getItem("usersM") || '[]'));
+
+    const _onMessageWasSent = (message) => {
+        setMessageList([...messageList, message]);
+        
+        localStorage.setItem('usersM', JSON.stringify(messageList));
+
+    }
+
+
+
+    return <div>
+
+        <button onClick={() => {
+
+            let userKaMessage = prompt("message ebntr kren")
+
+            let message = {
+                author: 'them',
+                type: 'text',
+                data: { text: userKaMessage }
+            }
+
+            setMessageList([...messageList, message]);
+            localStorage.setItem('usersM', JSON.stringify(messageList));
+
+
+
+
+        }}>set data</button>
+
+        
+        <h1>{name}</h1>
+        <BrowserRouter>
+
+
+            <Routes>
+
+            </Routes>
+        </BrowserRouter>
+
+        <Launcher
+            agentProfile={{
+                teamName: 'anc',
+                imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+            }}
+            onMessageWasSent={_onMessageWasSent}
+            messageList={messageList}
+            showEmoji
+        />
+
+        
+    </div>;
+
 }
 
-export default App;
